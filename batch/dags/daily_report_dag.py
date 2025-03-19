34e155bcf72393f232dd04b2cb56a867a048bf18
honuuk <jewook3617@gmail.com>
2025-03-19 16:19:50 +0900

    하둡 관련 코드 제거

--- batch/dags/daily_report_dag.py
+++ batch/dags/daily_report_dag.py
@@ -21,24 +21,7 @@ with DAG(
     catchup=False,
     tags=['daily', 'report', 'spark']
 ) as dag:
-
-    submit_spark_job = BashOperator(
-        task_id='spark_daily_report',
-        bash_command=(
-            'echo "Spark 작업 시작" && '
-            'poetry run python /home/honuuk/ssafy-custom-news-data/batch/spark_daily_report.py --date {{ ds }} &&'
-            'echo "Spark 작업 완료"'
-        )
-    )
-
-    notify_report_generated = BashOperator(
-        task_id='notify_report_generated',
-        bash_command=(
-            'echo "리포트가 생성되었습니다: '
-            'report/daily_report_{{ ds_nodash }}.pdf"'
-        )
-    )
-
+    
     data_sync_job = BashOperator(
         task_id='hourly_data_sync',
         bash_command=(
@@ -48,7 +31,7 @@ with DAG(
         )
     )
 
-    submit_spark_job >> notify_report_generated >> data_sync_job
+    data_sync_job
 
 if __name__ == "__main__":
     dag.test()
